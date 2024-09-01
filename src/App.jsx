a9bccf9d552c33216fc39838d3600310daad4d7b
Saurav kumar jha <skjsaurav1@gmail.com>
2024-09-01 20:19:06 +0530

    ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react'
+import React,{Fragment} from 'react'
 import './App.css'
 
 function App() {
   return (
-      <div className="box">
-        <img src="download.jfif" alt="download.jfif" />
+  <>
+    <Netfilxseries img="download.jfif" rating="8" />
+    <Netfilxseries img="download.jfif" rating="0"/>
+    <Welcome/>
+  </>
+
+  );
+}
+const Netfilxseries = (props)=>{
+  const Summary = ()=>{
+    const p = "Queen of Tears is a South Korean drama series that blends romance, family dynamics, and emotional tension. The story revolves around Baek Hyun-woo, a small-town lawyer, and his wife, Hong Hae-in, a chaebol heiress who leads the Queens Group, a powerful conglomerate. Despite their seemingly fairytale marriage, the couple faces severe marital crises, largely driven by grief over a lost child, miscommunication, and their different ways of handling pain."
+    return p;
+  };
+  return (
+    <>
+    <div className="box">
+        <img src={props.img} alt="download.jfif" />
         <h2>Name: QUEEN OF TEARS</h2>
-        <h3>Rating: 8.3/10</h3>
-        <p>Summary: Queen of Tears is a 2024 South Korean television series written by Park Ji-eun, co-directed by Jang Young-woo [ko] and Kim Hee-won, and starring Kim Soo-hyun and Kim Ji-won. The series depicts the story of a married couple in a crisis. It aired on tvN from March 9, to April 28, 2024, every Saturday and Sunday at 21:20 (KST). It is also available for streaming on TVING in South Korea, and on Netflix in selected regions</p>
+        <h3>Rating: {(props.rating )+ 2}/10</h3>
+        <p><strong>SUMMARY:</strong>{Summary()}</p>
       </div>
+    </>
   )
+}
+
+function Welcome(){
+  const name = "SAURAV kr jha";
+  return (
+    <>
+       <h1>HELLO , {name} </h1>
+    </>
+  );
 }
 
 export default App
